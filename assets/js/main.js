f7da5b02e8dc716029c4abcfc8563825fb888640
anh1511 <anhn20089@gmail.com>
2023-04-23 19:38:03 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,29 @@ function closeForm() {
    document.getElementById("myForm").style.display = "none";
 }
 
+// Open Nav mobile
+// Đóng/mở menu
+const nav = document.getElementById('nav');
+const navMobiMenu = document.querySelector('.nav-mobi-menu');
+
+navMobiMenu.addEventListener('click', function() {
+  nav.classList.toggle('nav-open');
+});
+
+// // Tự động đóng menu
+// var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+// for (var i=0; i<menuItems.length; i++) {
+//     var menuItem = menuItems[i];
+    
+//     menuItem.onclick =function(Event) {
+//         var isParenMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('nav-menu');
+//         if (isParenMenu) {
+//             event.preventDefault();
+//         }else {
+//             nav.style.height = null;
+//         }
+//     }
+// }
 
 // Slideshow
 let slideIndex = 0;
@@ -90,6 +113,92 @@ function checkInput() {
   };  
 
 
-// Xóa sản phẩm trong giỏ hàng
 
 
+// Tìm kiếm và lọc sản phẩm
+const categoryItems = document.querySelectorAll('.aside-item');
+const productItems = document.querySelectorAll('.item');
+const titleItems = document.querySelectorAll('.title-items');
+
+categoryItems.forEach((categoryItem) => {
+    categoryItem.addEventListener('click', () => {
+        const category = categoryItem.getAttribute('data-category');
+        filterProducts(category);
+    });
+});
+
+function filterProducts(category) {
+    productItems.forEach((productItem) => {
+        const productCategory = productItem.getAttribute('data-category');
+        if (category === productCategory || category === 'all') {
+            productItem.style.display = 'block';
+            productItem.style.width = '225px';
+        } else {
+            productItem.style.display = 'none';
+        }
+    });
+}
+
+
+const searchInput = document.querySelector('.search-in');
+
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    productItems.forEach((productItem) => {
+        const productTitle = productItem.querySelector('.item-heading').textContent.toLowerCase();
+        if (productTitle.includes(searchTerm)) {
+            productItem.style.display = 'block';
+            productItem.style.width = '250px';
+            titleItems.forEach((titleItem) => {
+              titleItem.style.display = 'none';
+            });
+            const relatedProducts= document.querySelector('.related-products');
+            relatedProducts.style.display = 'block';
+        } else {
+            productItem.style.display = 'none';
+            titleItems.forEach((titleItem) => {
+              titleItem.style.display = 'none';
+            });
+        }
+    });
+});
+
+
+
+// CHATBOT
+const chatboxButton = document.querySelector('#chatbox-button');
+const chatbox = document.querySelector('#chatbox');
+const chatboxClose = document.querySelector('.close');
+const chatboxMessages = document.querySelector('#chatbox-messages');
+const chatboxForm = document.querySelector('#chatbox-form');
+const chatboxInput = document.querySelector('#chatbox-input');
+
+// Hiển thị chatbox khi nhấp vào nút chatbox-button
+chatboxButton.addEventListener('click', () => {
+  chatbox.style.display = 'block';
+});
+
+// Đóng chatbox khi nhấp vào nút close
+chatboxClose.addEventListener('click', () => {
+  chatbox.style.display = 'none';
+});
+
+// Tự động trả lời tin nhắn
+chatboxForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Ngăn chặn sự kiện mặc định của form
+
+  const message = chatboxInput.value;
+  const reply = 'Tôi đang không trực tuyến, tôi sẽ trả lời bạn ngay khi tôi trở lại.';
+
+  // Thêm tin nhắn của người dùng vào khung chat
+  chatboxMessages.innerHTML += `<p><strong>Bạn:</strong> ${message}</p>`;
+
+  // Xóa nội dung của input và đặt trỏ lại vào input
+  chatboxInput.value = '';
+  chatboxInput.focus();
+
+  // Thêm phản hồi của chatbot vào khung chat
+  setTimeout(() => {
+    chatboxMessages.innerHTML += `<p><strong></strong> ${reply}</p>`;
+  }, 2000); // Chờ 2 giây để phản hồi của chatbot xuất hiện
+});
